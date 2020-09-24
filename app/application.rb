e85a389ddd06093bbff8705e7b125b17ae2c58d6
Harley Hanna <harleyhannawrit@gmail.com>
2020-09-24 20:09:48 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,8 +1,8 @@
 class Application
   def call(env)
     resp = Rack::Response.new
-    
-    if Time.now.to_i <= 12
+    time = Time.now
+    if time.hour <= 12
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
